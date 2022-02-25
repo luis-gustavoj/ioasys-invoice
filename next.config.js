51e0ff4dc9bc80a78264aa9392af7d44a394648e
Luis Silva <luisg.juliao@gmail.com>
2022-02-25 15:57:16 -0300

    feat: webpack convert camelCase classes to kebab-case

--- next.config.js
+++ next.config.js
@@ -7,6 +7,21 @@ module.exports = withReactSvg({
   reactStrictMode: true,
   include: path.resolve(__dirname, "src/assets/svg"),
   webpack(config, options) {
-    return config;
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
+        if (/css-loader[/\\](?:cjs|dist|src)/.test(moduleLoader.loader)) {
+          if (typeof moduleLoader.options.modules === "object") {
+            moduleLoader.options.modules = {
+              ...moduleLoader.options.modules,
+              exportLocalsConvention: "camelCaseOnly",
+            };
+          }
+        }
+      });
+    });
   },
 });
