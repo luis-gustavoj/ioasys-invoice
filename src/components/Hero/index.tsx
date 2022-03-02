6ca45d9dc8c702fc98a251250d42674128620fe1
Luis Silva <luisg.juliao@gmail.com>
2022-03-02 17:01:01 -0300

    feat: added raw hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export const Hero = () => {
+  return (
+    <section id="hero" className={styles.heroContainer}>
+      <div className={styles.textContainer}>
+        <h1>ioasys voice</h1>
+        <h2>O comportamento do usuario em plataformas digitais</h2>
+      </div>
+      <div className={styles.imageContainer}>
+        <div>
+          <Image
+            src="/hero-image.png"
+            height={507}
+            width={634}
+            alt="Hero image"
+          />
+        </div>
+        <div className={styles.smokyBackgroundImage}>
+          <Image
+            src="/smoky-bg.png"
+            height={628}
+            width={839}
+            alt="Background image"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
