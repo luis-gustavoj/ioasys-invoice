fdf08e4a56494bf94c590dd61ef88954da9d2082
Luis Silva <luisg.juliao@gmail.com>
2022-03-08 16:04:34 -0300

    feat: added title to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,28 @@
-import { AboutSection } from "src/components/About";
-import { ContactSection } from "src/components/Contact";
-import { Footer } from "src/components/Footer";
+import Head from "next/head";
+
 import { Header } from "src/components/Header";
 import { Hero } from "src/components/Hero";
+import { AboutSection } from "src/components/About";
 import { PeopleSection } from "src/components/PeopleSection";
+import { ContactSection } from "src/components/Contact";
+import { Footer } from "src/components/Footer";
 
 import styles from "./Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.pageWrapper}>
-      <Header />
-      <Hero />
-      <AboutSection />
-      <PeopleSection />
-      <ContactSection />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>ioasys voice</title>
+      </Head>
+      <div className={styles.pageWrapper}>
+        <Header />
+        <Hero />
+        <AboutSection />
+        <PeopleSection />
+        <ContactSection />
+        <Footer />
+      </div>
+    </>
   );
 }
